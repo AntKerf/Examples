97ba41e47f8fd172747bbe3d65b57ec2c4e7b178
Anton Lukyanov <nicelad47@gmail.com>
2020-12-24 22:23:54 +0700

    add source v1.0

---
+++ IFromBook/Source/main.cpp
@@ -0,0 +1,23 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QString>
+
+
+#include <QQmlContext>
+
+int main(int argc, char *argv[])
+{
+#if defined(Q_OS_WIN)
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+
+    QGuiApplication app(argc, argv);
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    engine.rootContext()->setContextProperty("applicationDirPath", QGuiApplication::applicationDirPath());
+
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    return app.exec();
+}
---
+++ IFromBook/readme.md
@@ -0,0 +1,7 @@
+Интерфейс приложения для оцифровки сканов.
+
+Создано в Qt, Qml.
+
+Сборка через qmake: qmake.exe -o Makefile "Source\IFromBook.pro"
+
+Скачать приложение: [![Actual](https://img.shields.io/badge/Build-1.0-green)](https://github.com/AntKerf/Examples/releases/tag/IFB_1.0)
